92842b855fe49e4419d7d02efb77cf5a3b6c562b
shravanibudh09-lang <shravanibudh09@gmail.com>
2026-08-04 10:53:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -387,7 +387,7 @@ def uploaded_file(filename):
 
 init_db()
 
-if _name_ == "_main_":
+if __name_ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
     app.run(debug=debug, host="0.0.0.0", port=port)
